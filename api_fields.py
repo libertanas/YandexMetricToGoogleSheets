ea3c8c50f718e0525d5a0af7fddc607294cd0b6d
libertanas <162423802+libertanas@users.noreply.github.com>
2025-09-16 11:08:02 +0300

    Update api_fields.py

--- api_fields.py
+++ api_fields.py
@@ -4,35 +4,27 @@ hit_field_list = [
     "ym:pv:URL",
     "ym:pv:deviceCategory",
     "ym:pv:operatingSystemRoot",
-    "ym:pv:clientID",
     "ym:pv:browser",
     "ym:pv:lastTrafficSource",
-    "ym:pv:goalsID",
-    "ym:pv:parsedParamsKey1",
-    "ym:pv:parsedParamsKey2",
-    "ym:pv:referer"
+    "ym:pv:referer",
+    "ym:pv:title",
+    "ym:pv:from",
+    "ym:pv:lastTrafficSource",
+    "ym:pv:regionCity"
 ]
 
 visit_field_list = [
     "ym:s:date",
     "ym:s:dateTime",
     "ym:s:pageViews",
-    "ym:s:visitDuration",
     "ym:s:startURL",
     "ym:s:deviceCategory",
     "ym:s:operatingSystemRoot",
-    "ym:s:clientID",
     "ym:s:browser",
     "ym:s:browserEngine",
-    "ym:s:screenOrientation",
-    "ym:s:screenWidth",
-    "ym:s:screenHeight",
-    "ym:s:physicalScreenWidth",
-    "ym:s:physicalScreenHeight",
     "ym:s:lastTrafficSource",
     "ym:s:purchaseRevenue",
     "ym:s:purchaseID",
     "ym:s:bounce",
-    "ym:s:isNewUser",
     "ym:s:goalsID"
 ]
